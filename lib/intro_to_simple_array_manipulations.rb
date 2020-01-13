1814ca9fb7809b007bd531ec05426e4099786603
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-13 05:25:20 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,52 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop_(array)
+  removed1 = array.pop
+  removed1
+end
+
+def pop_with_args(array)
+  removed2 = array.pop)(2)
+  removed2
+end
+
+def using_shift(array)
+  removed3 = array.shift
+  removed3
+end
+
+def shift_with_args(array)
+  removed4 = array.shift(2)
+  removed4
+end
+
+
+def using_concat(array, array)
+  
+end
+
+def using_insert(array, element)
+  
+end
+
+def using_uniq(array)
+  
+end
+
+def using_flatten(array)
+  
+end
+
+def using_delete(array, string)
+  
+end
+
+def using_delete_at(array, integer)
+  
+end
